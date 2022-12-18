0f217978bd1e9bf9981aa132ccbf241fc643b5c7
Igor Encarnação Junqueira Castro <igor.castro@trescon.com.br>
2022-12-18 08:17:40 -0300

    criando o metodo post

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -3,11 +3,20 @@ package com.api.parkingcontrol.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @Service
 public class ParkingSpotService {
 	
 	@Autowired
-	ParkingSpotRepository parkingSpotService;
+	ParkingSpotRepository parkingSpotRepository;
+	
+	@Transactional
+	public Object save(@Valid ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
 }
